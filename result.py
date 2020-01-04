16c96f014c29255550ec3b44d64fa95668c3056c
alexander <alexander.koronovskiy@gmail.com>
2020-01-05 02:32:53 +0400

    normal example

--- result.py
+++ result.py
@@ -2,5 +2,5 @@ import load
 import matplotlib.pyplot as plt
 from diff_ex import *
 
-s = load.load_series(generator='diff_sol', x=[1, -0.2, -0.15], f=[f_x, f_y, f_t], pt=2000, dx=0.05)
+s = load.load_series(generator='diff_sol', x=[0.1, 0.2, 0.7], f=[f_x, f_y, f_t], pt=1000, dx=0.001)
 plt.plot(s); plt.show()
