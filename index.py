b7dabb0b2aef2b92f53cbee8eb05117034c87816
alexander <alexander.koronovskiy@gmail.com>
2021-03-08 00:33:21 +0400

    trying js 2 + 404

--- index.py
+++ index.py
@@ -8,13 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     time_series = os.listdir("data_raw")
-    images = os.listdir("static/images")
     return render_template(
-        "index.html",
-        time_series=time_series,
-        images=reversed(images),
-        title="Extended DFA transform",
+        "index.html", time_series=time_series, title="Extended DFA transform"
     )
+
+
+@app.route("/graphics")
+def result():
+    images = os.listdir("static/images")
+    return render_template("graphics.html", images=reversed(images))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,12 @@
 $('.vertical-menu > a').click(function() {
-  if ($(this).attr('id') === 'name') {
-    alert($(this).text())
-  }
-})
+    if ($(this).attr('id') === 'file') {
+        var path = $(this).text();
+
+        $.ajax({
+            url: "/graphics",
+            type: "get",
+            data: {jsdata: path},
+            success: function(response) {$("#main-content").html(response);}
+        });
+    }
+});
